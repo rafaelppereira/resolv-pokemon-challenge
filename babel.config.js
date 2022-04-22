8b8c24dec8f8a774a36fd44ebacfc7894ff5e768
Rafael <rafaelsantospereira03@gmail.com>
2022-04-22 14:37:38 -0300

    test

--- babel.config.js
+++ babel.config.js
@@ -2,4 +2,3 @@ module.exports = {
   presets: ["next/babel"],
   plugins: [["styled-components", { "ssr": true }]]
 }
-''
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,48 +1,15 @@
-import React from 'react'
-import Document, {
-  DocumentInitialProps,
-  DocumentContext,
-  Html,
-  Head,
-  Main,
-  NextScript
-} from 'next/document'
-import { ServerStyleSheet } from 'styled-components'
+import Document, { Html, Head, Main, NextScript } from 'next/document'
 
 export default class MyDocument extends Document {
-  static async getInitialProps(
-    ctx: DocumentContext
-  ): Promise<DocumentInitialProps> {
-    const sheet = new ServerStyleSheet()
-    const originalRenderPage = ctx.renderPage
-
-    try {
-      ctx.renderPage = () =>
-        originalRenderPage({
-          enhanceApp: App => props => sheet.collectStyles(<App {...props} />)
-        })
-
-      const initialProps = await Document.getInitialProps(ctx)
-      return {
-        ...initialProps,
-        styles: (
-          <>
-            {initialProps.styles}
-            {sheet.getStyleElement()}
-          </>
-        )
-      }
-    } finally {
-      sheet.seal()
-    }
-  }
-
-  render(): JSX.Element {
+  render() {
     return (
-      <Html lang="pt">
+      <Html>
         <Head>
-          <meta charSet="utf-8" />
-          <link rel="icon" href="favicon.png" />
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet" />
+
+          <link rel="shortcut icon" href="/favicon.png" type="image/x-icon" />
         </Head>
         <body>
           <Main />
